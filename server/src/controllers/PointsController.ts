26cb32735897001f3a61258b6a41f5f3a17ad871
leonardo <leolub150@gmail.com>
2020-06-02 17:59:33 -0300

    default image

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -51,7 +51,7 @@ class PointsController {
     const trx = await knex.transaction()
 
     const point = {
-      image: 'image-fake',
+      image: 'https://images.unsplash.com/photo-1591099429057-b2c57ced7326?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       name,
       email,
       whatsapp,
