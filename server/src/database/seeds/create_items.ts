fefc74a6b2dab1420b4215101f6630a63b87da21
leonardo <leolub150@gmail.com>
2020-06-02 13:14:12 -0300

    db items seeds

---
+++ server/src/database/seeds/create_items.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import Knex from 'knex'
+
+export async function seed (knex: Knex) {
+  await knex('items').insert([
+    { title: 'Lâmpadas', image: 'lampadas.svg' },
+    { title: 'Pilhas e Bateria', image: 'bateria.svg' },
+    { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
+    { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
+    { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
+    { title: 'Óleo de cozinha', image: 'oleo.svg' }
+  ])
+}
