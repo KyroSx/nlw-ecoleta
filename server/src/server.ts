4a8a89be8cd6085346522c4594a7745fcd059f92
leonardo <leolub150@gmail.com>
2020-06-02 13:14:43 -0300

    serving static files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import path from 'path'
 import routes from './routes'
 
 const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+const staticFiles = path.resolve(__dirname, '..', 'uploads')
+app.use('/uploads', express.static(staticFiles))
 
 app.listen(3333)
