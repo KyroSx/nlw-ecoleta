f71480c0949242aed8f572185977590c6617d4ac
leonardo <leolub150@gmail.com>
2020-06-03 12:58:15 -0300

    first page base

---
+++ web/src/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './styles.css'
+
+import logo from '../../assets/logo.svg';
+
+const Home = () => {
+  return (
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={logo} alt="logo"/>
+        </header>
+
+        <main>
+          <h1>Seu Marketplace de coleta de resíduos</h1>
+          <p>
+            Ajudamos as pessoas a encontrarem pontos de coleta
+            de forma eficiente
+          </p>
+
+          <a href="/cadastro">
+            <span>></span>        
+            <strong>Cadastre um ponto de coleta</strong>
+          </a>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
