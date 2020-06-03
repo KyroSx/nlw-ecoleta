f21eb73607b6b937dd2f94f05bdbea1ab00a98d7
leonardo <leolub150@gmail.com>
2020-06-03 14:16:43 -0300

    get items from api

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,28 @@
-import  React from 'react'
+import  React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowDownLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet'
 
 import './styles.css'
 import logo from '../../assets/logo.svg'
+import api from '../../services/api'
+
+interface Item {
+  id: number,
+  title: string,
+  image_url: string
+}
 
 const CreatePoint = () => {
+
+  const [items, setItems] = useState<Item[]>([])
+
+  useEffect(() => {
+    api.get('items').then(response => {
+      setItems(response.data)
+    })
+  }, [])
+
   return (
     <div id="page-create-point">
       <header>
@@ -94,10 +110,15 @@ const CreatePoint = () => {
           </legend>
 
           <ul className="items-grid">
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg " alt="oleo"/>
-              <span>oleo de cozinha</span>
-            </li>
+            {
+              items.map((item) => (
+                <li key={item.id}>
+                  {console.log(item.image_url)}
+                  <img src={item.image_url} alt={item.title}/>
+                  <span>{item.title}</span>
+                </li>
+              ))
+            }
           </ul>
         </fieldset>
 
