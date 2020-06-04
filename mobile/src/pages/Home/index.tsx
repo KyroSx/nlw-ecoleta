cd35f00a5feeefbf56222988cb9773fa2c91981d
leonardo <leolub150@gmail.com>
2020-06-04 10:48:31 -0300

    home page assets

---
+++ mobile/src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Image, View } from 'react-native'
+
+const Home = () => {
+  return (
+    <>
+      <View>
+        <Image source={require('../../assets/logo.png')} />
+      </View>
+    </>
+  )
+}
+
+export default Home
